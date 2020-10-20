42c13dbee1fc98dfc8bae499cca196f7d609e660
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-10-19 22:12:13 -0300

    feat: repositorio para categoria

---
+++ src/main/java/com/service/ecommerce/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.service.ecommerce.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.service.ecommerce.models.Categoria;
+
+public interface CategoriaRepository extends CrudRepository<Categoria, Integer> {
+
+}
