eda7213d6f6d6d3d679932230f1b4191cb788c72
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-10-21 22:56:54 -0300

    feat: repositorios tipo crudRepository

---
+++ src/main/java/com/service/ecommerce/repositories/MarcaRepository.java
@@ -0,0 +1,9 @@
+package com.service.ecommerce.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.service.ecommerce.models.Marca;
+
+public interface MarcaRepository extends CrudRepository<Marca, Integer> {
+
+}
