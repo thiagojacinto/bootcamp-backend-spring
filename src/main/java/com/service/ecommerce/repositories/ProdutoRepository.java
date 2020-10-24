cf2c9572c111cbcf3450eddd1b453009d0aa2026
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-10-24 15:19:26 -0300

    fix: método de ordenação por preço

--- src/main/java/com/service/ecommerce/repositories/ProdutoRepository.java
+++ src/main/java/com/service/ecommerce/repositories/ProdutoRepository.java
@@ -3,7 +3,6 @@ package com.service.ecommerce.repositories;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.service.ecommerce.models.Produto;
@@ -14,10 +13,9 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	/**
 	 * Lista de produtos ordenada pelo Preço Unitário. Usa {@code Pageable} e {@code Sort}
 	 * @param pageable
-	 * @param sort
 	 * @return
 	 */
-	List<Produto> findAllOrderByPreco_unitario(Pageable pageable, Sort sort);
+	List<Produto> findByOrderByPrecoUnitario(Pageable pageable);
 	
 	/**
 	 * Lista de produtos por fornecedor, ordenado por fornecedor. Usa {@code Pageable}
