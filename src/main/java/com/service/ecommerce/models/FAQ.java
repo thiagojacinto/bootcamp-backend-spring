6256665173011966accddef76c80126ac2becfc2
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-10-23 23:37:33 -0300

    feat: criação das entidades FAQ e Produto

---
+++ src/main/java/com/service/ecommerce/models/FAQ.java
@@ -0,0 +1,148 @@
+package com.service.ecommerce.models;
+
+import java.time.OffsetDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "faq")
+public class FAQ {
+	
+	@Id
+	@NotNull(groups = ValidationGroups.RegisterValidation.class)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull
+	private OffsetDateTime datahora;
+	
+	@NotNull
+	private String texto;
+	
+	@NotNull
+	@ManyToOne
+	private Produto produto;
+	
+	public FAQ() {
+		this.datahora = OffsetDateTime.now();
+	}
+	
+	public FAQ(String texto) {
+		this.datahora = OffsetDateTime.now();
+		this.texto = texto;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the datahora
+	 */
+	public OffsetDateTime getDatahora() {
+		return datahora;
+	}
+
+	/**
+	 * @param datahora the datahora to set
+	 */
+	public void setDatahora(OffsetDateTime datahora) {
+		this.datahora = datahora;
+	}
+
+	/**
+	 * @return the texto
+	 */
+	public String getTexto() {
+		return texto;
+	}
+
+	/**
+	 * @param texto the texto to set
+	 */
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	/**
+	 * @return the produto
+	 */
+	public Produto getProduto() {
+		return produto;
+	}
+
+	/**
+	 * @param produto the produto to set
+	 */
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FAQ other = (FAQ) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("FAQ [");
+		if (id != null) {
+			builder.append("id = ");
+			builder.append(id);
+			builder.append(", ");
+		}
+		if (datahora != null) {
+			builder.append("datahora = ");
+			builder.append(datahora);
+			builder.append(", ");
+		}
+		if (texto != null) {
+			builder.append("texto = ");
+			builder.append(texto);
+			builder.append(", ");
+		}
+		if (produto != null) {
+			builder.append("produto = ");
+			builder.append(produto);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+}
---
+++ src/main/java/com/service/ecommerce/models/Produto.java
@@ -0,0 +1,233 @@
+package com.service.ecommerce.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@NotNull(groups = ValidationGroups.RegisterValidation.class)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotBlank
+	@Size(max = 200)
+	private String nome;
+	private String descricao;
+	private Double preco_unitario;
+	
+	@Size(max = 20)
+	private String unidade;
+	
+	@ManyToOne
+	private Categoria categoria;
+	@ManyToOne
+	private Fornecedor fornecedor;
+	@ManyToOne
+	private Marca marca;
+	
+	public Produto() {}
+
+	/**
+	 * @param nome
+	 * @param descricao
+	 * @param preco_unitario
+	 */
+	public Produto(String nome, String descricao, Double preco_unitario) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco_unitario = preco_unitario;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+
+	/**
+	 * @param descricao the descricao to set
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	/**
+	 * @return the preco_unitario
+	 */
+	public Double getPreco_unitario() {
+		return preco_unitario;
+	}
+
+	/**
+	 * @param preco_unitario the preco_unitario to set
+	 */
+	public void setPreco_unitario(Double preco_unitario) {
+		this.preco_unitario = preco_unitario;
+	}
+
+	/**
+	 * @return the unidade
+	 */
+	public String getUnidade() {
+		return unidade;
+	}
+
+	/**
+	 * @param unidade the unidade to set
+	 */
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+
+	/**
+	 * @return the categoria
+	 */
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	/**
+	 * @param categoria the categoria to set
+	 */
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	/**
+	 * @return the fornecedor
+	 */
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	/**
+	 * @param fornecedor the fornecedor to set
+	 */
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+
+	/**
+	 * @return the marca
+	 */
+	public Marca getMarca() {
+		return marca;
+	}
+
+	/**
+	 * @param marca the marca to set
+	 */
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Produto [");
+		if (id != null) {
+			builder.append("id = ");
+			builder.append(id);
+			builder.append(", ");
+		}
+		if (nome != null) {
+			builder.append("nome = ");
+			builder.append(nome);
+			builder.append(", ");
+		}
+		if (descricao != null) {
+			builder.append("descricao = ");
+			builder.append(descricao);
+			builder.append(", ");
+		}
+		if (preco_unitario != null) {
+			builder.append("precoUnitario = ");
+			builder.append(preco_unitario);
+			builder.append(", ");
+		}
+		if (unidade != null) {
+			builder.append("unidade = ");
+			builder.append(unidade);
+			builder.append(", ");
+		}
+		if (categoria != null) {
+			builder.append("categoria = ");
+			builder.append(categoria);
+			builder.append(", ");
+		}
+		if (fornecedor != null) {
+			builder.append("fornecedor = ");
+			builder.append(fornecedor);
+			builder.append(", ");
+		}
+		if (marca != null) {
+			builder.append("marca = ");
+			builder.append(marca);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
