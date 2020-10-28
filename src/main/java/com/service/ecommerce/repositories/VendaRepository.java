da16104e193a40f6b091a0f548c06c42e88d1bb2
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-10-27 21:02:25 -0300

    fix: métodos com sub-atributos na interface JpaRepositort

--- src/main/java/com/service/ecommerce/repositories/VendaRepository.java
+++ src/main/java/com/service/ecommerce/repositories/VendaRepository.java
@@ -15,7 +15,7 @@ public interface VendaRepository extends JpaRepository<Venda, Integer> {
 	 * @param pageable
 	 * @return {@code List<Venda>}
 	 */
-	public List<Venda> findAllByClienteOrderByDatahoraDesc(Integer clienteID, Pageable pageable);
+	public List<Venda> findByCliente_IdOrderByDatahoraDesc(Integer clienteID, Pageable pageable);
 	
 	/**
 	 * Lista de Vendas por um cliente ordenado pelo ValorTotal de forma decrescente.
@@ -23,12 +23,12 @@ public interface VendaRepository extends JpaRepository<Venda, Integer> {
 	 * @param pageable
 	 * @return {@code List<Venda>}
 	 */
-	public List<Venda> findAllByClienteOrderByValorTotalDesc(Integer clienteID, Pageable pageable);
+	public List<Venda> findByCliente_IdOrderByValorTotalDesc(Integer clienteID, Pageable pageable);
 	
 	/**
 	 * Lista de Vendas a partir de uma forma de pagamento.
 	 * @param formaPagamentoID
 	 * @return {@code List<Venda>}
 	 */
-	public List<Venda> findAllByFormaPagamento(Integer formaPagamentoID);
+	public List<Venda> findByFormaPagamento_Id(Integer formaPagamentoID);
 }
