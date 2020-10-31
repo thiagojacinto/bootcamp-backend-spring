b5c12a0b08017dd172c846c56d68288f0b18851e
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-10-31 17:23:26 -0300

    refactor: uso do JsonIdentity para evitar Loop na desserialização

--- src/main/java/com/service/ecommerce/models/FAQ.java
+++ src/main/java/com/service/ecommerce/models/FAQ.java
@@ -10,7 +10,13 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 @Entity
+@JsonIdentityInfo(
+		  generator = ObjectIdGenerators.PropertyGenerator.class, 
+		  property = "id")
 @Table(name = "faq")
 public class FAQ {
 	
