c35142822f13d5ecc7eab8035f2663d2fc488749
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-01-21 23:40:02 -0300

    hotfix: ativando o CORS para chamadas do cliente Angular

--- src/main/java/com/service/ecommerce/EcommerceApplication.java
+++ src/main/java/com/service/ecommerce/EcommerceApplication.java
@@ -2,6 +2,9 @@ package com.service.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -10,4 +13,29 @@ public class EcommerceApplication {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				
+				registry.addMapping("/v1/**")
+					.allowedOrigins("http://localhost:4200")
+					.allowedMethods("*");
+				
+				registry.addMapping("/v1/**")
+					.allowedOrigins("https://dot-store-angular.netlify.app/")
+					.allowedMethods("*");
+				
+				registry.addMapping("/swagger-ui/**")
+					.allowedOrigins("*");
+				
+				registry.addMapping("/api/v2/api-docs")
+					.allowedOrigins("*");
+				
+			}
+			
+		};
+	}
 }
